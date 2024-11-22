918dcaa71806ab71ce48f7b48c4e5b0650a1e837
Dev_Soulboy <Ezepaulnwachukwu@gmail.com>
2024-11-22 18:03:03 +0100

    calculatorCompleted

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const inputtt = document.querySelector("input");
+let eqaulto = document.querySelector(".equalto");
+
+let displayer = function (num) {
+  inputtt.value += num;
+};
+
+eqaulto.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let a = inputtt.value;
+  let x = eval(a);
+  inputtt.value = x;
+});
+
+let clearr = function () {
+  inputtt.value = "  ";
+};
+let deletee = function () {
+  inputtt.value = inputtt.value.slice(0, -1)
+};
